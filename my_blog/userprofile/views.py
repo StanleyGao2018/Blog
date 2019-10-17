2daea32f88058c740aaa8b17c5cb1c1c71b65038
StanleyGao <gao2421688910@hotmail.com>
2019-10-17 09:11:14 +0000

    add useful tool

--- my_blog/userprofile/views.py
+++ my_blog/userprofile/views.py
@@ -75,4 +75,25 @@ def profile_edit(request, id):
     profile = Profile.objects.get(user_id=id)
 
     if request.method == 'POST':
-        
+        # 验证修改数据者,是否为本人
+        return HttpResponse("你没有权限修改此用户信息.")
+
+        profile_form = ProfileForm(data=request.POST)
+        if profile_form.is_valid():
+            # 取得清洗后的合法数据
+            profile_cd = profile_form.cleaned_data
+            profile.phone = profile_cd['photo']
+            profile.bio = profile_cd['bio']
+            profile.save()
+            # 带参数的redirect
+            return redirect("userprofile:edit", id=id)
+        else:
+            return HttpResponse("注册表输入有误。请重新输入~")
+
+    elif request.method == 'GET':
+        profile_form = ProfileForm()
+        context = { 'profile_form': profile_form, 'profile': profile, 'user': user}
+        return render(request, 'userprofile/edit.html', context)
+
+    else:
+        return HttpResponse("请使用GET或POSET请求数据")
